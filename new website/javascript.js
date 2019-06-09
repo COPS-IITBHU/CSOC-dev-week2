c7c42b2844980e5272e65773a90f9f25f69aa74c
unknown <dhirajtanwer.eee18@itbhu.ac.in>
2019-06-09 04:24:25 -0700

    assignment completed

---
+++ new website/javascript.js
@@ -0,0 +1,18 @@
+function slideshow() {
+	var x = document.getElementById('check-class');
+	if (x.style.display=== "none") {
+		x.style.display="block";
+	}
+	else{
+		x.style.display="none";
+	}
+}
+function course(){
+	var x= document.getElementById('cr');
+	if (x.style.display=== "none") {
+		x.style.display="block";
+	}
+	 else {
+		x.style.display="none";
+	}
+}
